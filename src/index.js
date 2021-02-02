fba4969ad43ab298c3e58284aad01cd72d6127fd
Dema Maksim <maksimdema@gmail.com>
2021-02-02 21:18:46 +0200

    update files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './styles.css';
+import templatesCountries from './templates/countries.hbs';
+
+const refs = {
+  searchForm: document.querySelector('.js-search-form'),
+  containerCountries: document.querySelector('.js-countries'),
+};
+
+refs.searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const form = refs.searchForm;
+  const inputValue = form.elements.query.value;
+
+  fetch(`https://restcountries.eu/rest/v2/name/${inputValue}`)
+    .then(res => res.json())
+    .then(name => {
+      console.log(name);
+      const markup = templatesCountries(name);
+
+      refs.containerCountries.insertAdjacentHTML('beforeend', markup);
+    });
+});
